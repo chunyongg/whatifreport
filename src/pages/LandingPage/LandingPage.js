61f3e1ea7f2c6e4d537cb5006029d7f8364932a2
neoruien <neoruien123@gmail.com>
2023-03-17 23:37:04 +0800

    Implement logging

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,28 +1,27 @@
-// Create a landing page
-// 1. Ask user for participant ID, this will be provided in Qualtrics
-// 2. After they click start, validate the participant ID is non-null, non-empty and is a string. If it fails, show an error message
-// 3. Call loggingjs.initialize() and pass in the participant ID
-// 4. Call loggingjs.initialize() with the following params: 'START', '1' 
-// 5. Redirect user to HomePage.js
-// Note: '1' must be a String, not Number
-// Once done, check https://docs.google.com/spreadsheets/d/11xAgg8_D9qaoKgkyHCbb88NgPFO0z3WqPhBT4Cw1H2A/edit?resourcekey#gid=114586490 to ensure your log is done
-// Try to make the style consistent with the rest of the What-if report
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import { useNavigate } from "react-router-dom";
 import styles from "./LandingPage.module.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
+import loggingjs from "../../logging";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   const [validated, setValidated] = useState(false);
+  const [participantId, setParticipantId] = useState("");
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
+    setValidated(true);
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
+      return;
     }
-    setValidated(true);
+    loggingjs.initialize(participantId);
+    loggingjs.logEvent("START", "1");
+    navigate("/home");
   };
 
   return (
@@ -33,12 +32,19 @@ function LandingPage() {
         onSubmit={handleSubmit}
         className={styles.form}
       >
-        <Form.Label style={{float: "left"}}>
+        <Form.Label style={{ float: "left" }}>
           Please enter your Participant ID (as provided by Qualtrics)
         </Form.Label>
-        <Form.Control required type="text" placeholder="Participant ID" />
-        <br/>
-        <Button type="submit" style={{width: "100%"}}>Submit form</Button>
+        <Form.Control
+          required
+          type="text"
+          placeholder="Participant ID"
+          onChange={(e) => setParticipantId(e.target.value)}
+        />
+        <br />
+        <Button type="submit" style={{ width: "100%" }}>
+          START
+        </Button>
       </Form>
     </div>
   );
