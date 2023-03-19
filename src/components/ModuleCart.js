0786b079531ca0ea6ac6a0c068059ac557283cc3
neoruien <neoruien123@gmail.com>
2023-03-19 12:49:04 +0800

    Solve eslint errors

--- src/components/ModuleCart.js
+++ src/components/ModuleCart.js
@@ -1,14 +1,9 @@
 import React from "react";
 import styles from "./ModuleCart.module.css";
-import LongYellowButton from "./LongYellowButton";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function ModuleCart({ modules }) {
-  const navigate = useNavigate();
-  const navigateBack = () => {
-    navigate(-1);
-  };
   const selectedModules = () => {
     return modules.length === 0 ? (
       <p>(None)</p>
@@ -26,11 +21,6 @@ function ModuleCart({ modules }) {
     <div className={styles.moduleCart}>
       <p className={styles.title}>Modules Selected:</p>
       <p className={styles.items}>{selectedModules()}</p>
-      {/* <p className={styles.footer}>
-        <LongYellowButton className={styles.btn} onClick={navigateBack}>
-          Next
-        </LongYellowButton>
-      </p> */}
     </div>
   );
 }
